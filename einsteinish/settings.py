dfc6963a1735b7b43e349193c51877dde9471202
Ki Hong <sfvue@aol.com>
2016-01-05 13:30:25 -0800

    minor changes

--- einsteinish/settings.py
+++ einsteinish/settings.py
@@ -186,7 +186,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'einsteinishuserdb',
         'USER': 'einsteinishuser',             # Not used with sqlite3.
-        'PASSWORD': 'HK*UY(!4akkb777',            # Not used with sqlite3.
+        'PASSWORD': 'password',            # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
